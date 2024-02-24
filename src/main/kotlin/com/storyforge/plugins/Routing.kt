6d7f6bca32f5f01b27f1b3a5cd21d80da4b90a75
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-24 17:34:52 +0500

    -Article Data Storage Completion (#16)

--- src/main/kotlin/com/storyforge/plugins/Routing.kt
+++ src/main/kotlin/com/storyforge/plugins/Routing.kt
@@ -1,5 +1,8 @@
 package com.storyforge.plugins
 
+import com.storyforge.plugins.domain.repository.ArticleRepository
+import com.storyforge.plugins.domain.repository.DatabaseFactory
+import com.storyforge.plugins.routes.article
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.autohead.*
@@ -8,15 +11,15 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    DatabaseFactory.init()
+    val db = ArticleRepository()
     install(StatusPages) {
         exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
         }
     }
     install(AutoHeadResponse)
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        article(db)
     }
 }
