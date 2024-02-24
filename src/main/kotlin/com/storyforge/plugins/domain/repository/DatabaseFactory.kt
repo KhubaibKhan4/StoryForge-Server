42953784987d4d16e95c9873c60359a20b9ab43d
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-02-24 16:21:41 +0500

    -Schema Utils Imp (#7)

--- src/main/kotlin/com/storyforge/plugins/domain/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/storyforge/plugins/domain/repository/DatabaseFactory.kt
@@ -1,11 +1,22 @@
 package com.storyforge.plugins.domain.repository
 
+import com.storyforge.plugins.data.local.ArticleTable
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
 
-    fun hikari(): HikariDataSource{
+    fun init() {
+        Database.connect(hikari())
+        transaction {
+            SchemaUtils.create(ArticleTable)
+        }
+    }
+
+    fun hikari(): HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = System.getenv("JDBC_DRIVER")
         config.jdbcUrl = System.getenv("JDBC_DATABASE_URL")
